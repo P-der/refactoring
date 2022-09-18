6c378440e6842e0ab7383b6a2589cc1cade5f1ed
pangdeen <pangdeen@authing.cn>
2022-09-19 00:52:44 +0800

    partOne: 分离子类特殊volumeCredits

--- PartOne/createStatementData.js
+++ PartOne/createStatementData.js
@@ -44,7 +44,6 @@ class PerformanceCalculator {
     get volumeCredits() {
         let result = 0
         result += Math.max(this.performance.audience - 30, 0)
-        if("comedy" === this.play.type) result += Math.floor(this.performance.audience/5)
         return result
     }
 }
@@ -66,5 +65,8 @@ class ComedyCalcuator extends PerformanceCalculator {
         }
         result += 300 * this.performance.audience
         return result
+    }
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience/5)
     }
 }
