160a48a380f9fcc1a761d19442bdba081ee63078
pangdeen <pangdeen@authing.cn>
2022-09-19 00:14:12 +0800

    partOne: 搬移renderPlainText中volumeCreditsFor函数

--- PartOne/statement.js
+++ PartOne/statement.js
@@ -7,6 +7,7 @@ function statement (invoices, plays) {
         const result = Object.assign({},aPerformance)
         result.play = playFor(result)
         result.amount = amountFor(result)
+        result.volumeCredits = volumeCreditsFor(result)
         return result
     }
     function playFor(aPerformance) {
@@ -34,6 +35,12 @@ function statement (invoices, plays) {
         }
         return result
     }
+    function volumeCreditsFor(aPerformance) {
+        let result = 0
+        result += Math.max(aPerformance.audience - 30, 0)
+        if("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience/5)
+        return result
+    }
 }
 function renderPlainText (data, plays) {
     let result = `Statement for ${data.customer}\n`
@@ -48,19 +55,14 @@ function renderPlainText (data, plays) {
     result += `You earned ${totalVolumeCredits()} credits\n`
     return result
 
-    function volumeCreditsFor(aPerformance) {
-        let result = 0
-        result += Math.max(aPerformance.audience - 30, 0)
-        if("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience/5)
-        return result
-    }
+    
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US", {style: 'currency', currency:"USD", minimumFractionDigits: 2}).format(aNumber/100)
     }
     function totalVolumeCredits() {
         let result = 0;
         for(let perf of data.performances) {
-            result += volumeCreditsFor(perf)
+            result += perf.volumeCredits
         }
         return result
     }
