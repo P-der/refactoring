ed473c7e45b3d37f22431767f8e43e23c81cc6f7
pangdeen <pangdeen@authing.cn>
2022-09-19 00:19:35 +0800

    partOne: 优化totalAmount和totalVolumeCredits函数

--- PartOne/statement.js
+++ PartOne/statement.js
@@ -44,18 +44,10 @@ function statement (invoices, plays) {
         return result
     }
     function totalVolumeCredits(data) {
-        let result = 0;
-        for(let perf of data.performances) {
-            result += perf.volumeCredits
-        }
-        return result
+        return data.performances.reduce((total, p)=> total + p.volumeCredits, 0)
     }
     function totalAmount(data) {
-        let result = 0;
-        for(let perf of data.performances) {
-            result += perf.amount
-        }
-        return result
+        return data.performances.reduce((total, p)=> total + p.amount, 0)
     }
 }
 function renderPlainText (data) {
