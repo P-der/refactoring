b5992031ac08070a98d7e6c8f58f93d21018c16d
pangdeen <pangdeen@authing.cn>
2022-09-19 00:51:26 +0800

    partOne: 分离子类 amount

--- PartOne/createStatementData.js
+++ PartOne/createStatementData.js
@@ -39,26 +39,7 @@ class PerformanceCalculator {
         this.play = aPlay
     }
     get amount() {
-        let result = 0
-        
-        switch(this.play.type) {
-            case "tragedy": 
-                result = 40000
-                if(this.performance.audience > 30) {
-                    result += 1000* (this.performance.audience - 30)
-                }
-                break
-            case "comedy": 
-                result = 30000
-                if(this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience -20)
-                }
-                result += 300 * this.performance.audience
-                break
-            default: 
-                throw new Error(`unknow type: ${this.play.type}`)
-        }
-        return result
+        throw new Error('subclass responsibility')
     }
     get volumeCredits() {
         let result = 0
@@ -68,5 +49,22 @@ class PerformanceCalculator {
     }
 }
 
-class TragedyCalcuator extends PerformanceCalculator {}
-class ComedyCalcuator extends PerformanceCalculator {}
+class TragedyCalcuator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000
+        if(this.performance.audience > 30) {
+            result += 1000* (this.performance.audience - 30)
+        }
+        return result
+    }
+}
+class ComedyCalcuator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000
+        if(this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience -20)
+        }
+        result += 300 * this.performance.audience
+        return result
+    }
+}
